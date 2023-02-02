7de863588ec5d53ab83eecf76cf363b536ef093e
Surapat Mekvanich <titleyugio@gmail.com>
2023-02-02 10:38:23 -0800

    commit tree 36460b8b315bbcd875ba8d62d162da6b1b1548d1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, include: :user, status: :ok
+        # render json: items, only: [:description, :name, :price], 
+        # include: [user: {only: [:username, :city]}], 
+        # status: :ok
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items, status: :ok 
+        # need to establish associations before using 'include'
+
+        # custom_response_hash = {
+        #     user: user,
+        #     items: user.items
+        # }
+        # render json: custom_response_hash
+
+        
+    end
 end
